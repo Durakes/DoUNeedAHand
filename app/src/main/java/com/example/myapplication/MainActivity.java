8c1da731b7732efed05dd9f45f98bd40c1430caf
Durakes <arnold.pajuelo@gmail.com>
2023-06-26 12:19:03 -0500

    Cambios

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        siguiente = findViewById(R.id.siguiente);
+        siguiente = findViewById(R.id.loginButton);
         siguiente.setOnClickListener(view -> {
             Toast.makeText(MainActivity.this, "Servicio!", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(MainActivity.this,Servicio.class));
--- app/src/main/java/com/example/myapplication/MenuFragment.java
+++ app/src/main/java/com/example/myapplication/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MenuFragment extends Fragment {
@@ -43,7 +45,14 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        Button btnEditPerfActivity = (Button) view.findViewById(R.id.limpiezas);
+        btnEditPerfActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditarPerfil.class);
+                startActivity(intent);
+            }
+        });
 }
